b09516ec1d0706e2eecc58e7016ae9385e8a226e
gkagklo <deathbreakerxx@hotmail.com>
2023-11-03 17:16:09 +0200

    likes & dislikes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,7 @@
     <!-- Font Awesome -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" integrity="sha256-KzZiKy0DWYsnwMF+X1DvQngQ2/FxF7MF3Ff72XcpuPs=" crossorigin="anonymous"></script>
     <!-- Scripts -->
+    @livewireStyles
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-gray-50 font-family-karla">
@@ -106,6 +107,6 @@
             <div class="uppercase py-6">&copy; myblog.com</div>
         </div>
     </footer>
-
+@livewireScripts
 </body>
 </html>
