deb5238bfbc7b18075cbb76b0aaafc3d700c1f3b
gkagklo <deathbreakerxx@hotmail.com>
2023-11-08 15:37:42 +0200

    recommended posts & recent categories

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Category extends Model
 {
@@ -14,6 +15,13 @@ class Category extends Model
     public function posts()
     {
         return $this->belongsToMany(Post::class);
+    }
+
+    public function publishedPosts()
+    {
+        return $this->belongsToMany(Post::class)
+        ->where('active', '=', 1)
+        ->whereDate('published_at', '<', Carbon::now());
     }
 
 }
